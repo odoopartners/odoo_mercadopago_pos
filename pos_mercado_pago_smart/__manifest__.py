640986f8c07a614659b0f56ddc64105519d35131
Marco A. Vasquez Levano <vasquezl.marco@gmail.com>
2025-05-13 18:24:13 -0500

    add pos_mercado_pago_smart

---
+++ pos_mercado_pago_smart/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    'name': 'POS Mercado Pago Smart',
+    'version': '18.0.1.0.1',
+    'author': 'Ganemo',
+    'website': 'https://www.ganemo.co',
+    'live_test_url': 'https://www.ganemo.co/demo',
+    'summary': 'Add payment support to Chile',
+    'description': """
+Add payment support to Chile.
+    """,
+    'module_type': 'official',
+    'category': 'Sales/Point of Sale',
+    'depends': ['pos_mercado_pago'],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizards/mp_access_token_views.xml',
+        'views/pos_order_views.xml'
+    ],
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'pos_mercado_pago_smart/static/**/*',
+        ],
+    },
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'license': 'Other proprietary',
+    'currency': 'USD',
+    'price': 0.00,
+}
