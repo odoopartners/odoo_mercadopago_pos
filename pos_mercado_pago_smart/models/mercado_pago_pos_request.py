2f2652836ffcbd46eeb1f3fb7c187523bb77d741
Marco Vasquez L. <46028657+mvasquez13@users.noreply.github.com>
2025-05-28 17:04:41 -0500

    [FIX] pos_mercado_pago_smart v18.0.1.0.1
    Archivo modificados:
     - pos_mercado_pago_smart/__init__.py
     - pos_mercado_pago_smart/__manifest__.py
     - pos_mercado_pago_smart/controllers/__init__.py
     - pos_mercado_pago_smart/controllers/main.py
     - pos_mercado_pago_smart/data/neutralize.sql
     - pos_mercado_pago_smart/models/__init__.py
     - pos_mercado_pago_smart/models/mercado_pago_pos_request.py
     - pos_mercado_pago_smart/models/pos_config.py
     - pos_mercado_pago_smart/models/pos_payment_method.py
     - pos_mercado_pago_smart/security/ir.model.access.csv
     - pos_mercado_pago_smart/static/src/app/payment_mercado_pago.js
     - pos_mercado_pago_smart/views/pos_order_views.xml
     - pos_mercado_pago_smart/wizards/__init__.py
     - pos_mercado_pago_smart/wizards/mp_access_token.py
     - pos_mercado_pago_smart/wizards/mp_access_token_views.xml
    
    **Tarea: https://www.ganemo.co/web#id=43697&view_type=form&model=project.task**

---
+++ pos_mercado_pago_smart/models/mercado_pago_pos_request.py
@@ -0,0 +1,42 @@
+from odoo.addons.pos_mercado_pago.models.mercado_pago_pos_request import MercadoPagoPosRequest, MERCADO_PAGO_API_ENDPOINT, REQUEST_TIMEOUT
+import logging
+import requests
+
+_logger = logging.getLogger(__name__)
+
+
+class MercadoPagoPosRequestExtended(MercadoPagoPosRequest):
+    def __init__(self, mp_bearer_token, idempotency_key=False):
+        super().__init__(mp_bearer_token)
+        self.idempotency_key = idempotency_key
+
+    def call_mercado_pago(self, method, endpoint, payload):
+        """ Make a request to Mercado Pago POS API.
+
+        :param method: "GET", "POST", ...
+        :param endpoint: The endpoint to be reached by the request.
+        :param payload: The payload of the request.
+        :return The JSON-formatted content of the response.
+
+        Note: The platform id below is not secret, and is just used to
+        quantify the amount of Odoo users on Mercado's backend.
+        """
+        endpoint = MERCADO_PAGO_API_ENDPOINT + endpoint
+        header = {}
+        if self.mercado_pago_bearer_token:
+            header['Authorization'] = f"Bearer {self.mercado_pago_bearer_token}"
+        if self.idempotency_key:
+            header['X-Idempotency-Key'] = self.idempotency_key
+        else:
+            header['X-platform-id'] = "dev_cdf1cfac242111ef9fdebe8d845d0987"
+
+        try:
+            response = requests.request(method, endpoint, headers=header, json=payload, timeout=REQUEST_TIMEOUT)
+            json_response = response.json()
+            return json_response
+        except requests.exceptions.RequestException as error:
+            _logger.warning("Cannot connect with Mercado Pago POS. Error: %s", error)
+            return {'errorMessage': str(error)}
+        except ValueError as error:
+            _logger.warning("Cannot decode response json. Error: %s", error)
+            return {'errorMessage': f"Cannot decode Mercado Pago POS response. Error: {error}"}
