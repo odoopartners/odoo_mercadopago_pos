8caf23a838155fb785e1f6b1884b7d3a770ba109
Marco Vasquez L. <46028657+mvasquez13@users.noreply.github.com>
2025-06-20 15:59:40 -0500

    Revert "[FIX] pos_mercado_pago_smart v18.0.1.0.1" (#8)
    Reverts odoopartners/odoo_mercadopago_pos#7

--- pos_mercado_pago_smart/static/src/overrides/components/payment_screen_payment_lines/payment_screen_lines.js
+++
@@ -1,9 +0,0 @@
-import { patch } from "@web/core/utils/patch";
-import { PaymentScreenPaymentLines } from "@point_of_sale/app/screens/payment_screen/payment_lines/payment_lines";
-
-patch(PaymentScreenPaymentLines, {
-    props: {
-        ...PaymentScreenPaymentLines.props,
-        getPaymentStatus: Function
-    }
-});
